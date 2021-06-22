eea73826b4711a690cc69ece667ed541a446d31f
Seongyoon-Kim <seongyoon5521@gmail.com>
2021-06-22 19:40:42 +0900

    풀백업

---
+++ client/ClientTest/src/main/webapp/javascript/ex04.js
@@ -0,0 +1,20 @@
+// ex04.js
+// - JavaScript Core 실행 가능
+// - JavaScript Browser 실행 불가능
+
+var num = 100;
+
+console.log('num', num);
+
+// Ctrl + F5
+// Ctrl + Alt + N
+
+if (num > 0) {
+    console.log('양수');
+} else {
+    console.log('음수');
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
